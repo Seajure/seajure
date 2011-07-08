4eb3d3d3e9223f71d5f7433c57ae19705e1bf558
VagrantUser <vagrant@natty.talifun.com>
2011-07-08 04:07:47 +0000

    Add swank-clojure to ring process.

--- project.clj
+++ project.clj
@@ -2,6 +2,8 @@
   :description "The web site for Seajure, the Seattle Clojure Group."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [enlive "1.0.0"]
+                 [compojure "0.6.2"]
+                 [swank-clojure "1.4.0-SNAPSHOT"]
                  [ring/ring-jetty-adapter "0.3.9"]]
   :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]]
   :main seajure.web)
--- src/seajure/server.clj
+++ src/seajure/server.clj
@@ -1,16 +1,39 @@
 (ns seajure.server
   (:use [ring.adapter.jetty]
         [ring.middleware.file :only [wrap-file]]
-        [seajure.web :only [index read-resource]]))
+        [compojure.core :only [defroutes GET PUT]]
+        [seajure.web :only [index read-resource]])
+  (:require [compojure.route :as route]
+            [swank.swank]
+            [clojure.string :as string]))
 
-(defn app [req]
+(defonce hellos (ref []))
+
+(defn list-hellos []
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body (interleave @hellos (repeat "\n"))})
+
+(defn home [req]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (index (read-resource "members.clj")
                 (read-resource "projects.clj"))})
 
+(defroutes app
+  (GET "/hellos" []
+       (list-hellos))
+  (PUT "/hellos/:text" [text]
+       (dosync (commute hellos conj text))
+       {:status 303
+        :headers {"Location" "/hellos"}})
+
+  (GET "/" [] home)
+  (route/not-found "Four Oh Four"))
+
 (defn -main []
   (let [port (Integer/parseInt (or (System/getenv "PORT") "5000"))]
-    (run-jetty (wrap-file app "resources") {:port port})))
+    (future (swank.swank/start-server))
+    (run-jetty (wrap-file app "resources") {:port 5001})))
 
 ;;; http://brehaut.net/blog/2011/ring_introduction
